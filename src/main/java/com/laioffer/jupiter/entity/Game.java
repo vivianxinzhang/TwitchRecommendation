4551e54840442d67a34278268d6a82574dd205d5
vxz <vivianxinzhang@gmail.com>
2020-11-28 15:49:20 -0800

    update GameServlet

--- src/main/java/com/laioffer/jupiter/entity/Game.java
+++ src/main/java/com/laioffer/jupiter/entity/Game.java
@@ -1,61 +1,88 @@
 package com.laioffer.jupiter.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Game {
-    private final String name;    // 初始化后不能修改
-    private String developer;   // no modifier, package private
-    private String releaseDate;
-    private float price;
+    // annotation
+    @JsonProperty("name")
+    public String name;
 
+    @JsonProperty("developer")
+    public String developer;
 
-    private Game(GameBuilder builder) {     // private constructor
-        this.name = builder.name;
-        this.developer = builder.developer;
-        this.releaseDate = builder.developer;
-        this.price = builder.price;
+    @JsonProperty("release_time")
+    public String releaseTime;
+
+    @JsonProperty("website")
+    public String website;
+
+    @JsonProperty("price")
+    public double price;
+
+    public Game(String name, String developer, String releaseTime, String website, double price) {
+        this.name = name;
+        this.developer = developer;
+        this.releaseTime = releaseTime;
+        this.website = website;
+        this.price = price;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public String getDeveloper() {
-        return developer;
-    }
-
-    public String getReleaseDate() {
-        return releaseDate;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public static class GameBuilder {   // GameBuilder使用 不依赖于Game对象 需要是static
-        private String name;
-        private String developer;   // no modifier, package private
-        private String releaseDate;
-        private float price;
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setDeveloper(String developer) {
-            this.developer = developer;
-        }
-
-        public void setReleaseDate(String releaseDate) {
-            this.releaseDate = releaseDate;
-        }
-
-        public void setPrice(float price) {
-            this.price = price;
-        }
-
-        public Game build() {
-            return new Game(this);  // pass in GameBuilder object
-        }
-    }
+//    private final String name;    // 初始化后不能修改
+//    private String developer;   // no modifier, package private
+//    private String releaseDate;
+//    private float price;
+//
+//
+//    private Game(GameBuilder builder) {     // private constructor
+//        this.name = builder.name;
+//        this.developer = builder.developer;
+//        this.releaseDate = builder.developer;
+//        this.price = builder.price;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getDeveloper() {
+//        return developer;
+//    }
+//
+//    public String getReleaseDate() {
+//        return releaseDate;
+//    }
+//
+//    public float getPrice() {
+//        return price;
+//    }
+//
+//    public static class GameBuilder {   // GameBuilder使用 不依赖于Game对象 需要是static
+//        private String name;
+//        private String developer;   // no modifier, package private
+//        private String releaseDate;
+//        private float price;
+//
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+//
+//        public void setDeveloper(String developer) {
+//            this.developer = developer;
+//        }
+//
+//        public void setReleaseDate(String releaseDate) {
+//            this.releaseDate = releaseDate;
+//        }
+//
+//        public void setPrice(float price) {
+//            this.price = price;
+//        }
+//
+//        public Game build() {
+//            return new Game(this);  // pass in GameBuilder object
+//        }
+//    }
 }
 // Game game = new Game("Vincent", "laioffer", "2020-11-28", "10");
 
--- src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
+++ src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
@@ -6,12 +6,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.apache.commons.io.IOUtils;
+import com.laioffer.jupiter.entity.Game;
 
 @WebServlet(name = "GameServlet", urlPatterns = {"/game"})  // 如果不写urlPattern在url中应该写ClassName：GameServlet
 public class GameServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 利用前端传入的数据 来初始化 JSONObject
+        // 前端发送的是JSON -- stream --> String --> JSON
+        // 互联网传输过程中 前端发送的 JSON 格式文件被转化为 stream 格式，可能一段JSON 需要几个 steam 才能收到
+        // Step 1: request.getReader() 获取 request的body, request的body本来是 stream 格式，流数据
+        // Step 2: IOUtils.toString() 将 request body转化为String
+        // Step 3: JSONObject 将String 转化为 JSON 格式的 Object
         JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
         String name = jsonRequest.getString("name");
         String developer = jsonRequest.getString("developer");
@@ -19,30 +28,46 @@ public class GameServlet extends HttpServlet {
         String website = jsonRequest.getString("website");
         float price = jsonRequest.getFloat("price");
 
+        // 此处把结果打印在console里
+        // 后面应该实现 save game to database
         System.out.println("Name is: " + name);
         System.out.println("Developer is: " + developer);
         System.out.println("Release time is: " + releaseTime);
         System.out.println("Website is: " + website);
         System.out.println("Price is: " + price);
 
-        response.setContentType("application/json");
+        response.setContentType("application/json");    // 给前端的注释 数据的返回格式
         JSONObject jsonResponse = new JSONObject();
-        jsonResponse.put("status", "ok");
+        jsonResponse.put("status", "ok");   // 这里不是必须的
         response.getWriter().print(jsonResponse);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // response.getWriter().print("Hello World!");
+
+        // 解析 URL 里的 gamename parameter
         // String gamename = request.getParameter("gamename");
+        // request.getParameter can only read parameter in url
+        // request.getParameter can only be used in doGet method
         // response.getWriter().print("Game is: " + gamename);
 
-        response.setContentType("application/json");
-        JSONObject game = new JSONObject();
-        game.put("name", "World of Warcraft");
-        game.put("developer", "Blizzard Entertainment");
-        game.put("release_time", "Feb 11, 2005");
-        game.put("website", "https://www.worldofwarcraft.com");
-        game.put("price", 49.99);
-        response.getWriter().print(game);
+        // Method 1:
+        // 返回 JSON 格式的数据给前端
+//        response.setContentType("application/json");
+//        JSONObject obj = new JSONObject();
+//        obj.put("name", "World of Warcraft");
+//        obj.put("developer", "Blizzard Entertainment");
+//        obj.put("release_time", "Feb 11, 2005");
+//        obj.put("website", "https://www.worldofwarcraft.com");
+//        obj.put("price", 49.99);
+//        response.getWriter().print(obj);  // 隐含调用了 game.toString()
+
+        // Method 2:
+        // 把Java格式的对象 convert成JSONObject的数据
+        Game game = new Game("World of Warcraft", "Blizzard Entertainment",
+                "Feb 11, 2005", "https://www.worldofwarcraft.com", 49.99);
+        // Jackson library 里的 ObjectMapper class 提供了转化 Java Object
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().print(mapper.writeValueAsString(game));
     }
 }
