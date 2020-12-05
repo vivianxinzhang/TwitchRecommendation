82e67dcfb5058e4d4b4266c834d7d7f47fb9956b
vxz <vivianxinzhang@gmail.com>
2020-12-05 13:19:30 -0800

    add MySQLConnection

---
+++ src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
@@ -0,0 +1,28 @@
+package com.laioffer.jupiter.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnection {
+    private final Connection conn;
+
+    public MySQLConnection() throws MySQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection(MySQLDBUtil.getMySQLAddress());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to connect to Database");
+        }
+    }
+
+    public void close() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
