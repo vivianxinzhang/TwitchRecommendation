69bddb7718ca891d5db35026b92c18074ad086f7
vxz <vivianxinzhang@gmail.com>
2020-12-05 13:42:45 -0800

    Implement setFavoriteItems, unsetFavoriteItems and saveItems

--- src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
+++ src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
@@ -1,7 +1,11 @@
 package com.laioffer.jupiter.db;
 
+import com.laioffer.jupiter.entity.Item;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class MySQLConnection {
     private final Connection conn;
@@ -23,6 +27,63 @@ public class MySQLConnection {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void setFavoriteItem(String userId, Item item) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+        saveItem(item);
+        String sql = "INSERT IGNORE INTO favorite_records (user_id, item_id) VALUES (?, ?)";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, userId);
+            statement.setString(2, item.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to save favorite item to Database");
+        }
+    }
+
+    public void unsetFavoriteItem(String userId, String itemId) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+        String sql = "DELETE FROM favorite_records WHERE user_id = ? AND item_id = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, userId);
+            statement.setString(2, itemId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to delete favorite item to Database");
+        }
+    }
+
+    public void saveItem(Item item) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+        String sql = "INSERT IGNORE INTO items VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, item.getId());
+            statement.setString(2, item.getTitle());
+            statement.setString(3, item.getUrl());
+            statement.setString(4, item.getThumbnailUrl());
+            statement.setString(5, item.getBroadcasterName());
+            statement.setString(6, item.getGameId());
+            statement.setString(7, item.getType().toString());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to add item to Database");
         }
     }
 }
