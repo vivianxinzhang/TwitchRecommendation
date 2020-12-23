44d1088d3d1dc254db697fbbfde26b960d905eb9
vxz <vivianxinzhang@gmail.com>
2020-12-22 18:40:04 -0800

    add recommendByTopGames/default and recommendItemByUser/favorite history

---
+++ src/main/java/com/laioffer/jupiter/recommendation/Recommendation.java
@@ -0,0 +1,5 @@
+package com.laioffer.jupiter.recommendation;
+
+public class Recommendation {
+
+}
---
+++ src/main/java/com/laioffer/jupiter/recommendation/RecommendationException.java
@@ -0,0 +1,7 @@
+package com.laioffer.jupiter.recommendation;
+
+public class RecommendationException extends RuntimeException {
+    public RuntimeException(String errorMessage) {
+        super(errorMessage);
+    }
+}
